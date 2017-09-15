936b85b3ffbeab80d7b27e8be448b27f2243b340
Unknown <furqankhaan@gmail.com>
2017-09-15 17:22:55 +0900

    initial commit
    complete Splash screen

---
+++ app/src/main/java/com/furq/popularmovies/MainActivity.java
@@ -0,0 +1,17 @@
+package com.furq.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by furqan.khan on 9/15/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
