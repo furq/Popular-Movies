cb6f1e8d5433bdb6ddbcfe3235ccfa1e33e0f121
Unknown <furqankhaan@gmail.com>
2017-09-15 17:22:55 +0900

    splash screen

---
+++ app/src/main/java/com/furq/popularmovies/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.furq.popularmovies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // time delay in ms
+    private int TIME_DELAY = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Show splash for specific interval of time.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over.
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_DELAY);
+    }
+}
